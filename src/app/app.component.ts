757578553a57d7de4f0d13e566d35eb5fa5c75a2
Fawzi Benamor <f.benamor@root.sa>
2018-10-31 15:40:41 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth=false;
+  // lastUpdate=new Date();
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+  appareils=[
+    {
+      name:"TV",
+      status:"eteint"
+    },
+    {
+      name:"PC",
+      status:"allumé"
+    },
+    {
+      name:"Game",
+      status:"eteint"
+    },
+  ];
+ 
+
+  constructor() {
+    
+   var x=setInterval(()=>{this.isAuth=true;},4000);
+  //  console.log("x",x);
+
+  }
+  onAllumer(){
+    // var i=0;
+    // setInterval(()=>{i++;console.log("tout allumer",i);},1000);
+    console.log("tout allumer");
+    this.isAuth=false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { TestCComponent } from './test-c/test-c.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent,
+    TestCComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
